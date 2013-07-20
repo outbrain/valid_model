d8cf41ec07e71da40e8739f46d9a31201c5c2433
Josh Forman <josh@yoshrote.com>
2013-07-19 21:03:48 -0400

    Fixed __init__'s __all__

--- valid_model/__init__.py
+++ valid_model/__init__.py
@@ -1,5 +1,6 @@
 from valid_model import descriptors
+from valid_model import validators
 from valid_model.base import Object
 from valid_model.exc import ValidationError
-__all__ = ['descriptors', 'Object', 'ValidationError']
+__all__ = ['descriptors', 'validators', 'Object', 'ValidationError']
 
